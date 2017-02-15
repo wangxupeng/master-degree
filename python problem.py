07e0a2c4a5e35876c89807ff9da2fd71287a2641
wangxupeng <279354244@qq.com>
2017-02-15 21:10:52 +0800

    Update python problem.py

--- python problem.py
+++ python problem.py
@@ -14,3 +14,26 @@ list1
 def MyFun((x, y), (a, b)):
     return x * y - a * b
 #the function is wrong, because we cannot use tuple to create functions. tuple is immutable
+
+#Q4
+def count(*x):
+    list1=[]
+    list2=[]
+    for i in x:
+        i+=1
+        list1.append(i)
+    print(list1)
+    for a in list1:
+        a+=1
+        list2.append(a)
+    print(list2)
+
+count(1,2,3)
+
+list1=["1","2","3"]
+list2=[]
+for i in list1:
+    a = float(i)
+    a+= 1
+    list2.append(a)
+print(list2)
