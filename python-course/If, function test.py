5d45210a7f297123862135081ca92448f7a7aa4d
wangxupeng <279354244@qq.com>
2017-02-27 09:51:44 +0800

    Update If, function test.py

--- python-course/If, function test.py
+++ python-course/If, function test.py
@@ -37,4 +37,13 @@ elif x==1:
 else:
         print ('More')
         ###############################################################
+words = ['cat','window','defenestrate']
+for w in range(0,len(words)):
+    if len(words[w])>6:
+        print(words[w],len(words[w]))
         
+
+for w in words[:]:
+    if len(w)>6:
+        words.insert(0,w)
+        print(words)
