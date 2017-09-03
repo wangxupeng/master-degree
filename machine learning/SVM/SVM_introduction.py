178c0c94e91b2288e50e7ac8e43a2a14b277f1ef
wangxupeng <279354244@qq.com>
2017-09-03 15:59:05 +0800

    Update SVM_introduction.py

--- machine learning/SVM/SVM_introduction.py
+++ machine learning/SVM/SVM_introduction.py
@@ -14,8 +14,10 @@ def get_data():
     return x,y
 
 def main():
+    mpl.rcParams['font.sans-serif'] = [u'SimHei']
+    mpl.rcParams['axes.unicode_minus'] = False
     x,y = get_data()
-    x=x[:,:2]
+    x=x[:,1:3]
     x_train,x_test,y_train,y_test = train_test_split(x,y,random_state=1,train_size=0.6)
     model = svm.SVC(C=0.1,kernel='linear',decision_function_shape='ovr')
     model.fit(x_train,y_train)
@@ -29,10 +31,23 @@ def main():
     t2 = np.linspace(x2_min, x2_max, 500)
     x1,x2 = np.meshgrid(t1,t2)
     x_show = np.stack((x1.flat,x2.flat),axis=1)
-  
-
-
-
+    grid_hat = model.predict(x_show)     
+    grid_hat = grid_hat.reshape(x1.shape)
+    cm_light = mpl.colors.ListedColormap(['#A0FFA0', '#FFA0A0', '#A0A0FF'])
+    cm_dark = mpl.colors.ListedColormap(['g', 'r', 'b'])
+    plt.figure()
+    plt.pcolormesh(x1,x2,grid_hat,cmap=cm_light)
+    plt.scatter(x[:,0],x[:,1],c=y,edgecolors='k',s=50,cmap=cm_dark)
+    plt.scatter(x_test[:,0],x_test[:,1],s=120, facecolors='none', zorder=10)
+    iris_feature = u'花萼长度', u'花萼宽度', u'花瓣长度', u'花瓣宽度'
+    plt.xlabel(iris_feature[1],fontsize=14)
+    plt.ylabel(iris_feature[2],fontsize=14)
+    plt.xlim(x1_min,x1_max)
+    plt.ylim(x2_min,x2_max)
+    plt.title(u'鸢尾花SVM二特征分类', fontsize=16)
+    plt.grid(b=True, ls=':')
+    plt.tight_layout(pad=1.5)
+    plt.show()
 
 if __name__ == '__main__':
     main()
