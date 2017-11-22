1267d60d39f9aa511e042c4b9aaf7aa71ad615a3
王旭鹏 <279354244@qq.com>
2017-11-22 10:38:05 +0800

    Create simple-example.py

---
+++ machine learning/RNN/simple-example.py
@@ -0,0 +1,25 @@
+import numpy as np
+import tensorflow as tf
+
+
+if __name__ == '__main__':
+    n_inputs = 3
+    n_neurons = 5
+    X0 = tf.placeholder(tf.float32, [None, n_inputs])
+    X1 = tf.placeholder(tf.float32, [None, n_inputs])
+    Wx = tf.Variable(tf.random_normal(shape=[n_inputs, n_neurons], dtype=tf.float32))
+    Wy = tf.Variable(tf.random_normal(shape=[n_neurons, n_neurons], dtype=tf.float32))
+    b = tf.Variable(tf.zeros([1, n_neurons], dtype=tf.float32))
+    Y0 = tf.tanh(tf.matmul(X0, Wx) + b)
+    Y1 = tf.tanh(tf.matmul(Y0, Wy) + tf.matmul(X1, Wx) + b)
+    init = tf.global_variables_initializer()
+
+    # Mini-batch: instance 0,instance 1,instance 2,instance 3
+    X0_batch = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8], [9, 0, 1]])  # t = 0
+    X1_batch = np.array([[9, 8, 7], [0, 0, 0], [6, 5, 4], [3, 2, 1]])  # t = 1
+    with tf.Session() as sess:
+        init.run()
+        Y0_val, Y1_val = sess.run([Y0, Y1], feed_dict={X0: X0_batch, X1: X1_batch})
+
+    print(Y0_val)
+    print(Y1_val)
