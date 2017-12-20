55be4ddba3ed47a363a5ad6ca751cf4f4cc02d63
王旭鹏 <279354244@qq.com>
2017-12-20 22:11:05 +0800

    Update matplotlib-Making stacked plots.py

--- python-plot/Making stacked plots/matplotlib-Making stacked plots.py
+++ python-plot/Making stacked plots/matplotlib-Making stacked plots.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 if __name__ == '__main__':
     warnings.simplefilter('ignore')
-    df = pd.read_csv(r'D:\references\Python_Data_Visualization_Cookbook_2nd-master\Chapter_03\ch03-energy-production.csv')
+    df = pd.read_csv(r'F:\python\python visualization\Python_Data_Visualization_Cookbook_2nd-master\Chapter_03\ch03-energy-production.csv')
 
     columns = ['Coal', 'Natural Gas (Dry)', 'Crude Oil', 'Nuclear Electric Power',
                'Biomass Energy', 'Hydroelectric Power', 'Natural Gas Plant Liquids',
@@ -15,12 +15,18 @@ if __name__ == '__main__':
     colors = ['darkslategray', 'powderblue', 'darkmagenta', 'lightgreen', 'sienna',
               'royalblue', 'mistyrose', 'lavender', 'tomato', 'gold']
 
-    plt.figure(figsize=(12,8))
-    polys = plt.stackplot(df['Year'], df[columns].values.T, colors=colors)
+    plt.figure(figsize=(10,6))
+    polys = plt.stackplot(df['Year'], df[columns].values.T, colors=colors,alpha=0.7)
 
     rectangles = []
     for poly in polys:
         rectangles.append(plt.Rectangle((0,0), 1, 1, fc=poly.get_facecolor()[0]))
+    """
+    The legend is not yet supported with stacked plots. We therefore use the commands plt.
+Rectangle() to create the legend's rectangles. Each rectangle's colors is specifed using
+poly.get_facecolor()[0], where poly is an element of the list of polygons created by
+the stackplot() command.
+    """
     legend = plt.legend(rectangles, columns, loc=3)
     frame = legend.get_frame()
     frame.set_color('white')
@@ -31,6 +37,5 @@ if __name__ == '__main__':
     plt.xticks(fontsize = 16)
     plt.yticks(fontsize = 16)
     plt.xlim(1973, 2014)
-
+    plt.tight_layout()
     plt.show()
-
